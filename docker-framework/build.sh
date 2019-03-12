4e69c69ef3367589e6999532f2acb2bf69a62737
root <root@ip-172-31-9-131.ap-southeast-1.compute.internal>
2019-03-12 12:31:08 +0000

    changes

--- docker-framework/build.sh
+++ docker-framework/build.sh
@@ -1,38 +1,139 @@
 #!/bin/bash
- 
-    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    SCRIPT_NAME=$(basename $0)
-    BUILD_DIR=${SCRIPT_DIR}
-    DOCKER_REG=${DOCKER_REG:-docker-repo:8081}
-    DOCKER_USR=${DOCKER_USR:-admin}
-    DOCKER_PSW=${DOCKER_PSW:-password}
-    GRADLE_REPO=${gradle-release}
-    DOCKER_REPO=${DOCKER_REPO:-docker}
-    WORKSPACE="/var/lib/jenkins/workspace/docker-framework/docker-framework"
-    ARTFACTORY_URL=${ARTFACTORY_URL:-http://172.31.8.155:8081/artifactory}
- 
-               
-    cd ${WORKSPACE}
-               
-    echo -e "\nDocker login"
- 
-    if [ ! -z "${DOCKER_REG}" ]; then
-    
-        if [ -z "${DOCKER_USR}" ] || [ -z "${DOCKER_PSW}" ]; then
-            errorExit "Docker credentials not set (DOCKER_USR and DOCKER_PSW)"
-        fi
-        docker login -u ${DOCKER_USR} -p ${DOCKER_PSW} ${DOCKER_REG}/${DOCKER_REPO}  || errorExit "Docker login to ${DOCKER_REG} failed"
-                               
-    else
-        echo "Docker registry not set. Skipping"
+
+
+DOCKER_REG=${DOCKER_REG:-docker-repo:8081}
+DOCKER_USR=${DOCKER_USR:-admin}
+DOCKER_PSW=${DOCKER_PSW:-password}
+GRADLE_REPO=${GRADLE_REPO:-gradle-release}
+DOCKER_STAGE_REPO=${DOCKER_STAGE_REPO:-docker-stage-local}
+DOCKER_PROD_REPO=${DOCKER_PROD_REPO:-docker-prod-local}
+DOCKER_TAG={$DOCKER_TAG:-docker-framework}
+WORKSPACE="/var/lib/jenkins/workspace/docker-framework/docker-framework"
+ARTFACTORY_URL=${ARTFACTORY_URL:-http://172.31.8.155:8081/artifactory}
+TOMCAT_REPO=${TOMCAT_REPO:-tomcat}
+
+errorExit () {
+      echo -e "\nERROR: $1"; echo
+      exit 1
+}
+
+usage () {
+      cat << END_USAGE
+
+      ${SCRIPT_NAME} - Script for building the Gradle-Build web application in Ubuntu, Tomcat Container with Docker image and Kubernetes Helm chart
+
+      Usage: ./${SCRIPT_NAME} <options>
+
+      --build             : [optional] Build the Docker image
+      --push              : [optional] Push the Docker image
+
+      -h | --help         : Show this usage
+
+      END_USAGE
+
+          exit 1
+}
+
+# Docker login
+dockerLogin () {
+      echo -e "\nDocker login"
+      cd ${WORKSPACE}
+      if [ ! -z "${DOCKER_REG}" ]; then
+
+          if [ -z "${DOCKER_USR}" ] || [ -z "${DOCKER_PSW}" ]; then
+              errorExit "Docker credentials not set (DOCKER_USR and DOCKER_PSW)"
+          fi
+          docker login -u ${DOCKER_USR} -p ${DOCKER_PSW} ${DOCKER_REG}/${DOCKER_REPO}  || errorExit "Docker login to ${DOCKER_REG} failed"
+                                 
+      else
+          echo "Docker registry not set. Skipping"
+      fi
+}
+
+# Build Docker images
+buildDockerImage () {
+
+      echo -e "\nBuilding ${DOCKER_REPO}/${DOCKER_TAG}:latest and pushing to the docker registry"
+      echo -e "\n"
+      
+      echo -e "\nDownload Tomcat and Java from Tomcat Repository"
+      
+      curl -u ${DOCKER_USR}:${DOCKER_PSW} $ARTFACTORY_URL/${TOMCAT_REPO}/apache-tomcat-8.0.32.tar -o ${WORKSPACE}/apache-tomcat-8.0.32.tar.gz           
+      curl -u ${DOCKER_USR}:${DOCKER_PSW} $ARTFACTORY_URL/${TOMCAT_REPO}/jdk-8u91-linux-x64.tar -o ${WORKSPACE}/jdk-8u91-linux-x64.tar       
+
+      echo -e "\n"
+      echo -e "\nBuilding Docker Container and Pushing to Stage Repository "
+                          
+      docker build -t ${DOCKER_REG}/${DOCKER_REPO}/${DOCKER_TAG}:latest ${WORKSPACE} || errorExit "Building ${DOCKER_REPO}/${DOCKER_REPO}/docker-framework:${DOCKER_TAG} failed"           
+      docker tag ${DOCKER_REG}/${DOCKER_REPO}/${DOCKER_TAG}:latest ${DOCKER_REG}/${DOCKER_STAGE_REPO}/docker-framework:latest          
+      
+}
+
+# Push Docker images
+buildProdDockerImage () {
+      docker tag ${DOCKER_REG}/${DOCKER_STAGE_REPO}/docker-framework:latest  ${DOCKER_REG}/${DOCKER_PROD_REPO}/${DOCKER_TAG}:latest
+      docker push ${DOCKER_REG}/${DOCKER_PROD_REPO}/${DOCKER_TAG}:latest || errorExit "Pushing ${DOCKER_REPO}:${DOCKER_TAG} failed"
+}
+
+
+processOptions () {
+    if [ $# -eq 0 ]; then
+        usage
     fi
- 
-    echo -e "\nBuilding ${DOCKER_REPO}/docker-framework:latest and pushing to the docker registry"
-    curl -u ${DOCKER_USR}:${DOCKER_PSW} $ARTFACTORY_URL/tomcat/apache-tomcat-8.0.32.tar -o $BUILD_DIR/apache-tomcat-8.0.32.tar.gz           
-    curl -u ${DOCKER_USR}:${DOCKER_PSW} $ARTFACTORY_URL/tomcat/jdk-8u91-linux-x64.tar -o $BUILD_DIR/jdk-8u91-linux-x64.tar                           
-    docker build -t ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:latest ${WORKSPACE} || errorExit "Building ${DOCKER_REPO}/${DOCKER_REPO}/docker-framework:${DOCKER_TAG} failed"           
-    docker tag ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:latest ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:latest          
-    docker push ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:latest
- 
-    
- 
+
+    while [[ $# > 0 ]]; do
+        case "$1" in
+            --build)
+                BUILD="true"; shift
+            ;;
+            --push)
+                PUSH="true"; shift
+                
+            --registry)
+                DOCKER_REG=${2}; shift 2
+            ;;
+            --docker_usr)
+                DOCKER_USR=${2}; shift 2
+            ;;
+            --docker_psw)
+                DOCKER_PSW=${2}; shift 2
+            ;;
+            --tag)
+                DOCKER_TAG=${2}; shift 2  
+            ;;
+            -h | --help)
+                usage
+            ;;
+            *)
+                usage
+            ;;
+        esac
+    done
+}
+
+main () {
+    echo -e "\nRunning"
+
+    echo "DOCKER_REG:   ${DOCKER_REG}"
+    echo "DOCKER_USR:   ${DOCKER_USR}"
+    echo "DOCKER_REPO:  ${DOCKER_REPO}"
+    echo "DOCKER_TAG:   ${DOCKER_TAG}"
+
+    # Cleanup
+
+    # Build and push docker images if needed
+    if [ "${BUILD}" == "true" ]; then
+        buildDockerImage
+    fi
+    if [ "${PUSH}" == "true" ]; then
+        # Attempt docker login
+        dockerLogin
+        pushDockerImage
+    fi
+
+}
+
+############## Main
+
+processOptions $*
+main
---
+++ docker-framework/build.sh-bk
@@ -0,0 +1,53 @@
+#!/bin/bash
+ 
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    SCRIPT_NAME=$(basename $0)
+    BUILD_DIR=${SCRIPT_DIR}
+    DOCKER_REG=${DOCKER_REG:-docker-repo:8081}
+    DOCKER_USR=${DOCKER_USR:-admin}
+    DOCKER_PSW=${DOCKER_PSW:-password}
+    GRADLE_REPO=${gradle-release}
+    DOCKER_REPO=${DOCKER_REPO:-docker}
+    WORKSPACE="/var/lib/jenkins/gradle-framework"
+    ARTFACTORY_URL=${ARTFACTORY_URL:-http://172.31.8.155:8081/artifactory}
+ 
+               
+    cd ${WORKSPACE}
+               
+    echo -e "\nDocker login"
+ 
+    if [ ! -z "${DOCKER_REG}" ]; then
+    
+        if [ -z "${DOCKER_USR}" ] || [ -z "${DOCKER_PSW}" ]; then
+            errorExit "Docker credentials not set (DOCKER_USR and DOCKER_PSW)"
+        fi
+        docker login -u ${DOCKER_USR} -p ${DOCKER_PSW} ${DOCKER_REG}/${DOCKER_REPO}  || errorExit "Docker login to ${DOCKER_REG} failed"
+                               
+    else
+        echo "Docker registry not set. Skipping"
+    fi
+ 
+    echo -e "\nBuilding ${DOCKER_REPO}/docker-framework:${env.BUILD_NUMBER} and pushing to the docker registry"
+
+    curl -u ${DOCKER_USR}:${DOCKER_PSW} $ARTFACTORY_URL/tomcat/apache-tomcat-8.0.32.tar -o $BUILD_DIR/apache-tomcat-8.0.32.tar.gz
+               
+    curl -u ${DOCKER_USR}:${DOCKER_PSW} $ARTFACTORY_URL/tomcat/jdk-8u91-linux-x64.tar -o $BUILD_DIR/jdk-8u91-linux-x64.tar
+                               
+    docker build -t ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:${env.BUILD_NUMBER} ${WORKSPACE} || errorExit "Building ${DOCKER_REPO}/${DOCKER_REPO}/docker-framework:${DOCKER_TAG} failed"
+               
+    docker tag ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:${env.BUILD_NUMBER} ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:latest
+               
+    docker push ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:latest
+ 
+    echo -e "\nBuilding ${DOCKER_REPO}/docker-framework:${env.BUILD_NUMBER} and pushing to the docker registry"
+ 
+    cd ${WORKSPACE}/framework-test
+               
+    curl -u ${DOCKER_USR}:${DOCKER_PSW} $ARTFACTORY_URL/${GRADLE_REPO}/webservices.war
+               
+    docker build -t ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:${DOCKER_TAG} ${WORKSPACE}/framework-test || errorExit "Building ${DOCKER_REPO}/${DOCKER_REPO}/docker-framework:${DOCKER_TAG} failed"
+               
+    docker tag ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:${DOCKER_TAG} ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:latest
+               
+    docker push ${DOCKER_REG}/${DOCKER_REPO}/docker-framework:latest
+ 
